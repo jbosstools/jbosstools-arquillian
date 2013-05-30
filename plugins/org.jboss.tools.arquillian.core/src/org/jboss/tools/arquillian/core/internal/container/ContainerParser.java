792208f98d215c619e5e62eab6921974ec6cc936
Snjezana Peco <snjezana.peco@redhat.com>
2013-05-30 21:44:53 +0200

    JBIDE-14507 - arquillian excessive dependencies includes jsf, seam,project examples and portlets

--- plugins/org.jboss.tools.arquillian.core/src/org/jboss/tools/arquillian/core/internal/container/ContainerParser.java
+++ plugins/org.jboss.tools.arquillian.core/src/org/jboss/tools/arquillian/core/internal/container/ContainerParser.java
@@ -10,23 +10,30 @@
  ************************************************************************************/
 package org.jboss.tools.arquillian.core.internal.container;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.jboss.forge.arquillian.container.Container;
 import org.jboss.tools.arquillian.core.ArquillianCoreActivator;
-import org.jboss.tools.project.examples.model.ProjectExampleUtil;
+import org.jboss.tools.foundation.core.ecf.URLTransportUtility;
 import org.osgi.framework.Bundle;
-
 /**
  * 
  * @author snjeza
@@ -35,12 +42,24 @@ import org.osgi.framework.Bundle;
 public class ContainerParser {
 	
 	private static final String CONTAINERS_JSON = "https://raw.github.com/forge/plugin-arquillian/master/src/main/resources/containers.json"; //$NON-NLS-1$
+	private static final String PROTOCOL_FILE = "file"; //$NON-NLS-1$
+
+	private static final String PROTOCOL_PLATFORM = "platform"; //$NON-NLS-1$
 
 	private static List<Container> containers;
 	
+	/**
+	 * Refresh parser
+	 * 
+	 */
 	public static void refresh() {
 		containers = null;
 	}
+	
+	/**
+	 * Returns list of containers
+	 * 
+	 */
 	public static List<Container> getContainers() {
 		if (containers != null) {
 			return containers;
@@ -62,8 +81,7 @@ public class ContainerParser {
 
 	private static URL getUrl() {
 		try {
-			File f = ProjectExampleUtil.getProjectExamplesFile(new URL(CONTAINERS_JSON),
-					"containers", "json", new NullProgressMonitor()); //$NON-NLS-1$ //$NON-NLS-2$
+			File f = getFile(new URL(CONTAINERS_JSON), "containers", "json", new NullProgressMonitor()); //$NON-NLS-1$ //$NON-NLS-2$
 			if (f == null || !f.exists()) {
 				return getUrlFromBundle();
 			} else {
@@ -90,5 +108,73 @@ public class ContainerParser {
 		}
 		return null;
 	}
+	
+	private static File getFile(URL url, String prefix,
+			String suffix, IProgressMonitor monitor) {
+		File file = null;
+		if (PROTOCOL_FILE.equals(url.getProtocol())
+				|| PROTOCOL_PLATFORM.equalsIgnoreCase(url.getProtocol())) {
+			try {
+				file = new File(new URI(url.toExternalForm()));
+			} catch (Exception e) {
+				file = new File(url.getFile());
+			}
+			if (!file.exists())
+				return null;
+		} else {
+			try {
+				if (monitor.isCanceled()) {
+					return null;
+				}
+				long urlModified = -1;
+				file = getFile(url);
+				try {
+					urlModified = new URLTransportUtility()
+							.getLastModified(url);
+				} catch (CoreException e) {
+					if (file.exists()) {
+						return file;
+					}
+				}
+				if (file.exists()) {
+					long modified = file.lastModified();
+					if (modified > 0 && urlModified == modified) {
+						return file;
+					}
+				}
+				boolean fileAlreadyExists = file.exists();
+				file.getParentFile().mkdirs();
+				if (monitor.isCanceled()) {
+					return null;
+				}
+				BufferedOutputStream destination = new BufferedOutputStream(
+						new FileOutputStream(file));
+				IStatus result = new URLTransportUtility().download(prefix,
+						url.toExternalForm(), destination, monitor);
+				if (!result.isOK()) {
+					ArquillianCoreActivator.getDefault().getLog().log(result);
+					if (!fileAlreadyExists && file.exists()) {
+						file.delete();
+					}
+					return null;
+				} else {
+					if (file.exists() && urlModified > 0) {
+						file.setLastModified(urlModified);
+					}
+				}
+			} catch (FileNotFoundException e) {
+				ArquillianCoreActivator.log(e);
+				return null;
+			}
+		}
+		return file;
+	}
 
+	private static File getFile(URL url) {
+		IPath location = ArquillianCoreActivator.getDefault().getStateLocation();
+		File root = location.toFile();
+		String urlFile = url.getFile();
+		File file = new File(root, urlFile);
+		return file;
+	}
 }
